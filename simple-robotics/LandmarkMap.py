0776e32ef1b3b5785147c6a1af3d5b0837e21e89
Markus-Heidemann <marheidemann@googlemail.com>
2019-08-13 22:40:59 +0200

    formatting

--- simple-robotics/LandmarkMap.py
+++ simple-robotics/LandmarkMap.py
@@ -3,6 +3,7 @@ from math import sqrt
 from copy import copy
 from collections import namedtuple
 
+
 class LandmarkMap:
     def __init__(self, x=[], y=[], lm_id=[], color='C5', marker='o'):
         self.plot_color = color
@@ -18,12 +19,14 @@ class LandmarkMap:
 
     def __str__(self):
         ret_str = ""
-        for x,y,lm_id in zip(self.x, self.y, self.lm_id):
-            ret_str += "ID: {0:>3},\tX: {1:>8},\tY: {2:>8}\n".format(lm_id, x, y)
+        for x, y, lm_id in zip(self.x, self.y, self.lm_id):
+            ret_str += "ID: {0:>3},\tX: {1:>8},\tY: {2:>8}\n".format(
+                lm_id, x, y)
         return ret_str
 
     def plot(self):
-        plt.scatter(self.x, self.y, color=self.plot_color, marker=self.plot_marker)
+        plt.scatter(self.x, self.y, color=self.plot_color,
+                    marker=self.plot_marker)
 
     def addLandmarks(self, x_list, y_list):
         assert(len(x_list) == len(y_list))
@@ -45,8 +48,10 @@ class LandmarkMap:
                 y_in_range.append(copy(y))
         return x_in_range, y_in_range
 
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
