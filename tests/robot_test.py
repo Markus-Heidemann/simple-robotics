66dff8f75d1821166f4a1b3138d1e0984595b7d7
Markus-Heidemann <marheidemann@googlemail.com>
2019-08-13 22:43:48 +0200

    moved test of calculate_new_pose to test of VelocityMotionModel

--- tests/robot_test.py
+++ tests/robot_test.py
@@ -5,23 +5,19 @@ from math import pi
 from context import Robot
 from Robot import Robot, Pose
 
+
 class TestRobot(unittest.TestCase):
     def setUp(self):
         self.robot = Robot()
 
-    def test_calculate_new_pose(self):
-        pose = Pose()
-        new_pose = self.robot.calculateNewPose(pose, 1.0, 0.0, 2)
-        self.assertEqual(2.0, new_pose.x)
-        self.assertEqual(0.0, new_pose.y)
-        self.assertEqual(0.0, new_pose.theta)
-
     def test_move_straight(self):
         self.robot.move(0.0, 0.5*pi)
         self.robot.move(1.0, 0.0)
+
         self.assertEqual(0.0, self.robot.pose.x)
         self.assertEqual(1.0, self.robot.pose.y)
         self.assertEqual(0.5*pi, self.robot.pose.theta)
+
 
 if __name__ == '__main__':
     unittest.main()
